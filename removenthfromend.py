f8596209fce277f8bf351466ec5ea992a890d2af
Nelson <neldecas12@gmail.com>
2023-10-26 17:34:39 -0500

    removefromend

--- removenthfromend.py
+++ removenthfromend.py
@@ -8,5 +8,52 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        pass
+    def __init__(self) -> None:
+        self.array = []
+        
+
+    def createNode(self):
+        temp = None
+
+        for i in self.array[::-1]:
+            if temp == None:
+                temp = ListNode(i)
+            else:
+                node = ListNode(i)
+                node.next = temp
+                temp = node
+        return temp
+
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        node = head
+        index = 0
+
+        while node:
+            
+            self.array.append(node.val)
+            index += 1
+            node = node.next
+
+        self.array.pop(index - n)
+
+        return self.createNode()
+
+
+temp = None
+
+for i in [1,2,3,4,5]:
+    if temp == None:
+        temp = ListNode(i)
+    else:
+        node = ListNode(i)
+        node.next = temp
+        temp = node
+
+
+s = Solution()
+a = s.removeNthFromEnd(temp,2)
+
+while a:
+    print(a.val)
+    
+    a = a.next
