ebf62e73121fb50496ff37c8fc19cc67c497e639
Nelson <neldecas12@gmail.com>
2023-11-08 19:06:43 -0500

    method

--- divide_two_integers.py
+++ divide_two_integers.py
@@ -1,11 +1,10 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        if divisor == 0 or dividend ==0:
+        if divisor == 0 or dividend == 0:
             return 0
 
         if dividend == -(2**31) and divisor == -1:
             return 2**31 - 1
-
 
         if divisor > 0 and (dividend - divisor) - 1 > divisor:
             return self.divide(dividend - divisor, divisor)
@@ -13,10 +12,10 @@ class Solution:
             return self.divide(dividend - (-1 * divisor), divisor)
 
         if divisor > 0:
-            return (abs(dividend) - divisor) - 1
+            return (dividend - divisor) - 1
         else:
-            return dividend + divisor
+            return dividend + 1
 
 
 s = Solution()
-print(s.divide(1, -1))
+print(s.divide(7, -3))
